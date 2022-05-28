bf7c991269de63c3eb482172e8de77ce4f3ec8f8
mateusz-uran <mateusz.uranowski@onet.pl>
2022-05-28 12:12:08 +0200

    Create project structure

---
+++ src/main/java/io/github/mateuszuran/PTD/Manager/PtdManagerApplication.java
@@ -0,0 +1,13 @@
+package io.github.mateuszuran.PTD.Manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PtdManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PtdManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/mateuszuran/PTD/Manager/PtdManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.mateuszuran.PTD.Manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PtdManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
