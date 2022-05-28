442591fa2e5632def19ea5b8b09c4195b01ed89c
mateusz-uran <mateusz.uranowski@onet.pl>
2022-05-28 12:55:21 +0200

    Add home page template and main app controller

---
+++ src/main/java/io/github/mateuszuran/PTD/Manager/AppController.java
@@ -0,0 +1,13 @@
+package io.github.mateuszuran.PTD.Manager;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AppController {
+
+    @GetMapping("/home")
+    public String showHomePage() {
+        return "index";
+    }
+}
